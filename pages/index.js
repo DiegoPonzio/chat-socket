3ee3f77a6e44e83ff52bafe64df95a71c1bfd287
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-17 16:29:56 -0600

    holi

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import React from "react";
 import {Chat} from "../components/chat";
 import {Layout} from "../components/Layout";
 import withSession from "../lib/session";
